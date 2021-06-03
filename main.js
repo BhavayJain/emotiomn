60cf76bfe2258020ab58450532236d7a253e56ac
BhavayJain <77540919+BhavayJain@users.noreply.github.com>
2021-06-03 20:46:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+prediction1="";
+prediction2="";
+
+Webcam.set({
+
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+}
+);
+
+camera=document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+Webcam.snap(function(data_uri)
+{
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+}
+);
+}
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Sscb3W_Y1/model.json',modelloaded);
+    function modelloaded(){
+        console.log("modelloaded");
+    }
+
+    function speak(){
+        synth=window.speechSynthesis;
+        speak_data1="The First Prediction Is"+prediction1;
+        speak_data2="The Second  Prediction Is"+prediction2;
+        var utterThis=new SpeechSynthesisUtterance(speak_data1+speak_data2);
+        synth.speak(utterThis);
+    }
+
+function check(){
+img=document.getElementById("captured_image");
+classifier.classify(img,gotresult);
+}
+function gotresult(error,results){
+if(error){
+    console.log(error);
+}
+else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction1=results[0].label;
+    prediction2=results[1].label;
+    speak();
+    if(results[0].label=="Happy"){
+        document.getElementById("result_emoji").innerHTML="&#128522;";
+    }
+
+    if(results[0].label=="Sad"){
+        document.getElementById("result_emoji").innerHTML="&#128532;";
+    }
+    
+    if(results[0].label=="Angry"){
+        document.getElementById("result_emoji").innerHTML="&#128548;";
+    }
+
+    if(results[1].label=="Happy"){
+        document.getElementById("result_emoji2").innerHTML="&#128522;";
+    }
+
+    if(results[1].label=="Sad"){
+        document.getElementById("result_emoji2").innerHTML="&#128532;";
+    }
+    
+    if(results[1].label=="Angry"){
+        document.getElementById("result_emoji2").innerHTML="&#128548;";
+    }
+
+}
+
+
+    
+}
